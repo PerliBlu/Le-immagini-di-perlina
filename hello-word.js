deb7e6ae5c5da99d76058178081e7e4c1d55e10c
Catherin Cruz <cruz.catherin@gmail.com>
2017-04-18 15:34:33 -0500

    Modified hello world index page
    Modified hello world index page

--- hello-word.js
+++ hello-word.js
@@ -1,14 +1,14 @@
-// Load the http module to create an http server.
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World from Heroku!\n");
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        console.log(err);
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
-
-// Listen on port 8080, IP defaults to 127.0.0.1
-server.listen(8080);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8080/");
