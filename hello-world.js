7a0fdb1e5ba0a08aa64db840edce5c7110d32fda
twogg-git <cruz.catherin@gmail.com>
2017-04-18 22:17:25 -0500

    Modified hello-world file
    Modified hello-world file

--- hello-world.js
+++ hello-world.js
@@ -8,9 +8,6 @@ var port = process.env.PORT || 8000;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
 // set the home page route
 app.get('/', function(req, res) {
 
